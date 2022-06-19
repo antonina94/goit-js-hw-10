8c6d4545b105c453263086996857f21d4e6ff3e5
Antonina Bondar <tonya.bondar111@gmail.com>
2022-06-19 21:54:39 +0000

    fix render

--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
 import './css/styles.css';
 import './//css//fetchCountries.css';
 import { getRefs } from './/refs';
-import  {fetchCountries} from './/fetchCountries';
+import { fetchCountries } from './/fetchCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
-const refs = getRefs()
+const refs = getRefs();
 
 refs.input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
 function handleInput(event) {
   event.preventDefault();
   const inputCountry = event.target.value.trim();
-  if(inputCountry === ''){
-    refs.countryList.innerHTML = ''
-    refs.containerCountry.innerHTML = ''
-    return
+  if (inputCountry === '') {
+    refs.countryList.innerHTML = '';
+    refs.containerCountry.innerHTML = '';
+    return;
   }
-  fetchCountries(inputCountry).then(response => {
-    renderCountries(response);
-    if (response.length > 1 && response.length <= 10) {
-      renderCountries(response);
-    }
-    if (response.length > 10) {
-      Notify.info('Too many matches found. Please enter a more specific name.');
-    }
-    if (response.length === 1) {
-      renderCountryInfo(response);
-      clearInput();
-    }
-  }
-  )
-    .catch(error =>{
-        console.log(error)
+  fetchCountries(inputCountry)
+    .then(response => {
+      if (response.length >= 2 && response.length <= 10) {
+        renderCountries(response);
+      }
+      if (response.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (response.length === 1) {
+        renderCountryInfo(response);
+      }
+    })
+    .catch(error => {
+      console.log(error);
     });
+  clearInput();
 }
 
-
-function renderCountries(response){
+function renderCountries(response) {
   const murkup = response
     .map(country => {
       return `
@@ -68,9 +68,13 @@ function renderCountryInfo(response) {
                         }</h1>
                     </div>
                 </li>
-        <p class = 'country-capital'><span class = 'span-capital'>Capital: </span>${country.capital}
+        <p class = 'country-capital'><span class = 'span-capital'>Capital: </span>${
+          country.capital
+        }
         </p>
-        <p class = 'country-population'><span class = 'span-population'>Population: </span>${country.population}
+        <p class = 'country-population'><span class = 'span-population'>Population: </span>${
+          country.population
+        }
         </p>
         <p class = 'country-languages'><span class = 'span-languages'>Languages: </span>${Object.values(
           country.languages
@@ -83,9 +87,7 @@ function renderCountryInfo(response) {
 }
 
 function clearInput() {
-  if (renderCountryInfo) {
+  if (refs.input.value != '') {
     refs.countryList.innerHTML = '';
   }
 }
-
-
